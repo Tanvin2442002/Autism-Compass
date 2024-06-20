893ecb8144fe11b8a3dd2b5ca8da983566642b34
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-06-20 13:32:34 +0600

    add log in Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import LogIn from "./LogInRegForm/Login";
+import ParentReg from "./LogInRegForm/ParentReg";
 
 function App() {
   return (
-    <div className="landingPage">
-      <div className="LogInSignUpBtn">
-        <button>Log In</button>
-        <div className="signUpContainer">
-          <button className="signUpButton">Sign Up</button>
-          <div className="signUpOptions">
-            <button>Parent</button>
-            <button>Child</button>
-            <button>Doctor</button>
-            <button>Therapy Org.</button>
+    <Router>
+      <div className="landingPage">
+        <ul className="LogInSignUpBtn">
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+          <div className="signUpContainer">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <div className="signUpOptions">
+              <li>
+                <Link to="/signup/parent">Parent</Link>
+              </li>
+              <li>
+                <Link to="/signup/child">Child</Link>
+              </li>
+              <li>
+                <Link to="/signup/doctor">Doctor</Link>
+              </li>
+              <li>
+                <Link to="/signup/therapyorg">Therapy Org.</Link>
+              </li>
+            </div>
           </div>
+        </ul>
+        <div className="introduction">
+          <h2>Welcome to</h2>
+          <h1>AUTISM COMPASS</h1>
         </div>
+
+        <Routes>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup/parent" element={<ParentReg />} />
+        </Routes>
       </div>
-      <div className="introduction">
-        <h2>Welcome to</h2>
-        <h1>AUTISM COMPASS</h1>
-      </div>
-    </div>
+    </Router>
   );
 }
 
