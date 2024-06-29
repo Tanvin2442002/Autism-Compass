541c3282f2cda7f9b0a5ef701803355285a7f0df
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-06-29 13:17:07 +0600

    Frontend of user profile is ready and conneted to backend also

--- frontend/src/LogInSignUpComponent/Profile.js
+++ frontend/src/LogInSignUpComponent/Profile.js
@@ -2,14 +2,27 @@ import React, { useEffect, useState } from 'react';
 import './Profile.css';
 
 const Profile = () => {
-    const [Data, setData] = useState(null);
+    const [profileData, setProfileData] = useState({
+        NAME: '',
+        DOB: '',
+        AGE: '',
+        CONTACT_NO: '',
+        EMAIL: '',
+        P_EMAIL: '',
+        STREET: '',
+        CITY: '',
+        POSTAL_CODE: '',
+        DEGREE: '',
+        FIELD_OF_SPEC: '',
+        INSTITUTION: '',
+    });
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [gender, setGender] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
             const userDataString = localStorage.getItem('USER');
-            console.log(userDataString);
             if (!userDataString) {
                 setError('USER not found in local storage');
                 setLoading(false);
@@ -32,7 +45,20 @@ const Profile = () => {
                 }
 
                 const data = await response.json();
-                setData(data[0]); // Assuming the response is an array of user objects
+                setProfileData({
+                    NAME: data[0].NAME,
+                    DOB: data[0].DOB,
+                    AGE: data[0].AGE,
+                    CONTACT_NO: data[0].CONTACT_NO,
+                    EMAIL: data[0].EMAIL,
+                    P_EMAIL: data[0].P_EMAIL,
+                    STREET: data[0].STREET,
+                    CITY: data[0].CITY,
+                    POSTAL_CODE: data[0].POSTAL_CODE,
+                    DEGREE: data[0].DEGREE,
+                    FIELD_OF_SPEC: data[0].FIELD_OF_SPEC,
+                    INSTITUTION: data[0].INSTITUTION,
+                });
             } catch (err) {
                 setError(err.message);
             } finally {
@@ -43,24 +69,166 @@ const Profile = () => {
         fetchData();
     }, []);
 
+    const fetchGenderData = async () => {
+        const genderResponse = await fetch(`https://api.genderapi.io/api/?name=${encodeURIComponent(profileData.NAME)}&key=667faf277a781c44944e8b13`);
+        const genderData = await genderResponse.json();
+        if (genderData.status && genderData.gender !== 'null') {
+            setGender(genderData.gender === 'male' ? 'boy' : 'girl');
+        } else {
+            setGender('boy');
+        }
+        console.log('G', gender);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProfileData({
+            ...profileData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('Profile Data:', profileData);
+        // Call fetchGenderData function
+        await fetchGenderData();
+        // Add code here to handle form submission, e.g., send data to the server
+    };
+
+    const handleDelete = () => {
+        console.log('Delete Account');
+        // Add code here to handle account deletion
+    };
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className="user-info">
-            <h1>User Information</h1>
-            {Data && (
-                <>
-                    <div className="user-info-item"><strong>Name:</strong> {Data.NAME}</div>
-                    <div className="user-info-item"><strong>Date of Birth:</strong> {Data.DOB}</div>
-                    <div className="user-info-item"><strong>Contact Number:</strong> {Data.CONTACT_NO}</div>
-                    <div className="user-info-item"><strong>Email:</strong> {Data.EMAIL}</div>
-                    <div className="user-info-item"><strong>Personal Email:</strong> {Data.P_EMAIL}</div>
-                    <div className="user-info-item"><strong>City:</strong> {Data.CITY}</div>
-                    <div className="user-info-item"><strong>Street:</strong> {Data.STREET}</div>
-                    <div className="user-info-item"><strong>Postal Code:</strong> {Data.POSTAL_CODE}</div>
-                </>
-            )}
+        <div className="user-profile">
+            <div className="avatar">
+                <img src={`https://avatar.iran.liara.run/public/${gender}?username=${profileData.NAME}`} alt='Avatar' />
+            </div>
+            <div className="profile-info">
+                <h1>{profileData.NAME}</h1>
+            </div>
+            <form className="profile-form" onSubmit={handleSubmit}>
+                <div className="profile-form-group">
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        name="NAME"
+                        placeholder="Enter your Name"
+                        value={profileData.NAME}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Email Address</label>
+                    <input
+                        type="text"
+                        name="EMAIL"
+                        placeholder="Email Address"
+                        disabled
+                        value={profileData.EMAIL}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Contact Number</label>
+                    <input
+                        type="text"
+                        name="CONTACT_NO"
+                        placeholder="Enter contact number"
+                        value={profileData.CONTACT_NO}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Parent Email ID</label>
+                    <input
+                        type="email"
+                        name="P_EMAIL"
+                        disabled
+                        value={profileData.P_EMAIL}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Street</label>
+                    <input
+                        type="text"
+                        name="STREET"
+                        placeholder="Enter street"
+                        value={profileData.STREET}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>City</label>
+                    <input
+                        type="text"
+                        name="CITY"
+                        placeholder="Enter city"
+                        value={profileData.CITY}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Postal Code</label>
+                    <input
+                        type="number"
+                        name="POSTAL_CODE"
+                        placeholder="Enter postal code"
+                        value={profileData.POSTAL_CODE}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Institution</label>
+                    <input
+                        type="text"
+                        name="INSTITUTION"
+                        placeholder="Institution"
+                        value={profileData.INSTITUTION}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Degree</label>
+                    <input
+                        type="text"
+                        name="DEGREE"
+                        placeholder="Degree"
+                        value={profileData.DEGREE}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Field of Specialization</label>
+                    <input
+                        type="text"
+                        name="FIELD_OF_SPEC"
+                        placeholder="Field of specialization"
+                        value={profileData.FIELD_OF_SPEC}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="profile-form-group">
+                    <label>Age</label>
+                    <input
+                        type="text"
+                        name="AGE"
+                        placeholder="Enter your age"
+                        value={profileData.AGE}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="button-group">
+                    <button type="button" className="delete-account-btn" onClick={handleDelete}>Delete Account</button>
+                    <button type="submit" className="save-profile-btn">Save Profile</button>
+                </div>
+            </form>
         </div>
     );
 };
