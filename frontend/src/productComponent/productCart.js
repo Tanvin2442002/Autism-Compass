2e470b72fdb89f0887ef211ab71779549874a835
Md. Tanvin Sarkar Pallab <tanvin.pallab2442002@gmail.com>
2024-07-08 23:21:51 +0600

    Product cart details like fetching total price, removing from cart and adding to cart.

---
+++ frontend/src/productComponent/productCart.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import "./productCart.css";
+
+const ProductCart = () => {
+  // Initial state
+  const [cartItems, setCartItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [subtotal, setSubtotal] = useState(0);
+
+  const userData = JSON.parse(localStorage.getItem("USER"));
+  const userID = userData.ID;
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/products/detail/checkout?userID=${userID}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log("Fetched data:", data);
+        setCartItems(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProduct();
+    fetchSubtotal();
+  }, [userID]);
+
+  const fetchSubtotal = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/products/detail/checkout/total?userID=${userID}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log("Fetched data:", data);
+      setSubtotal(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!cartItems.length) return <div>Product not found</div>;
+
+  const handleproceedtopay = () => {
+    alert("Payment Successful");
+  };
+
+  const handleRemoveItem = async (PR_ID) => {
+    setLoading(true); // Set loading state when removing an item
+    try {
+      const response = await fetch(
+        `http://localhost:5000/products/detail/checkout?userID=${userID}&PR_ID=${PR_ID}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log("Fetched data:", data);
+      setCartItems(data);
+      fetchSubtotal(); // Call fetchSubtotal after removing an item
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="product-card-page">
+      <h1>Shopping Cart</h1>
+      <div className="product-show">
+        <div className="show-all">
+          {cartItems.filter(item => item !== null).map((cartItem) => (
+            <div className="box" key={cartItem.PR_ID}>
+              <img src={cartItem.SRC} alt={cartItem.NAME} />
+              <div className="content">
+                <h3>{cartItem.NAME}</h3>
+                <h4>Price: ${cartItem.AMOUNT}</h4>
+                <p className="unit">
+                  Quantity:
+                  <h3>{cartItem.QUANTITY}</h3>
+                </p>
+                <p className="btn-area">
+                  <i aria-hidden="true" className="fa fa-trash"></i>
+                  <span
+                    className="btn2"
+                    onClick={() => handleRemoveItem(cartItem.PR_ID)}
+                  >
+                    Remove
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="right-bar">
+          <p>
+            <span>Subtotal</span> <span>${subtotal.TOTAL}</span>
+          </p>
+          <p>
+            <span>Total</span> <span>${subtotal.TOTAL}</span>
+          </p>
+          <a href="#">
+            <i className="fa fa-shopping-cart" onClick={handleproceedtopay}></i>
+            Proceed to pay
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCart;
--- frontend/src/productComponent/productDetails.js
+++ frontend/src/productComponent/productDetails.js
@@ -1,82 +1,146 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import './ProductDetails.css'; // Import the CSS file
-import { useLocation } from "react-router-dom";
+import "./ProductDetails.css"; // Import the CSS file
+import { useLocation, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProductDetails = () => {
-    const [product, setProduct] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [quantity, setQuantity] = useState(1);
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [quantity, setQuantity] = useState(1);
 
-    const location = useLocation();
-    const params = new URLSearchParams(location.search);
-    const productType = params.get('ID');
+  const location = useLocation();
+  const navigate = useNavigate();
+  const params = new URLSearchParams(location.search);
+  const productType = params.get("ID");
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const response = await fetch(`http://localhost:5000/products/detail?ID=${productType}`);
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                const data = await response.json();
-                console.log('Fetched data:', data);
-                setProduct(data);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+  const userData = JSON.parse(localStorage.getItem("USER"));
 
-        fetchProduct();
-    }, [productType]);
-
-    const addCart = (productType) => {
-        console.log(`Product ${productType} with quantity ${quantity} added to cart`);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/products/detail?ID=${productType}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log("Fetched data:", data);
+        setProduct(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
-    if (!product) return <div>Product not found</div>;
-        
-     const totalPrice = product.PRICE * quantity;
+    fetchProduct();
+  }, [productType]);
 
-    return (
-        <div className="product-details">
-            <div className="product-image">
-                <img 
-                    src={product.SRC || "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"} 
-                    alt={product.NAME} 
-                />
-            </div>
-            <div className="product-info">
-                <h1>{product.NAME}</h1>
-                <div className="product-meta">
-                    <span>Price: ${product.PRICE}</span>
-                    <span>Total: ${totalPrice}</span>
-                </div>
-                <div className="product-description">
-                    <p>{product.DESCRIPTION}</p>
-                </div>
-                <div className="quantity-selector">
-                    <label htmlFor="quantity">Quantity:</label>
-                    <input
-                        type="number"
-                        id="quantity"
-                        name="quantity"
-                        min="1"
-                        value={quantity}
-                        onChange={(e) => setQuantity(parseInt(e.target.value))}
-                    />
-                </div>
-                <button className="cart-button" onClick={() => addCart(product.PR_ID)}>
-                    Add to cart
-                </button>
-            </div>
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!product) return <div>Product not found</div>;
+
+  const totalPrice = product.PRICE * quantity;
+
+  const addCart = async (PR_ID,quantity) => {
+    const purchaseData = {
+      P_ID: userData.ID,
+      PR_ID: PR_ID,
+      QUANTITY: quantity,
+      AMOUNT: totalPrice,
+      DOB: new Date().toISOString().slice(0, 10),
+    };
+    try {
+      const response = await fetch("http://localhost:5000/products/detail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(purchaseData),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log("Cart data:", data);
+
+      if (data.message === "Product added to cart successfully") {
+        toast.success("Product added to cart successfully", {
+          position: "top-right",
+          autoClose: 2500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const handleCheckout = () => {
+      navigate("/products/detail/checkout");
+  };
+
+  const handleContinueShopping = () => {
+    navigate("/products");
+  };
+
+  return (
+    <div className="product-details">
+      <ToastContainer />
+      <div className="product-image">
+        <img
+          src={
+            product.SRC ||
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+          }
+          alt={product.NAME}
+        />
+      </div>
+      <div className="product-info">
+        <h1>{product.NAME}</h1>
+        <div className="product-meta">
+          <span>Price: ${product.PRICE}</span>
+          <span>Total: ${totalPrice}</span>
         </div>
-    );
+        <div className="product-description">
+          <p>{product.DESCRIPTION}</p>
+        </div>
+        <div className="quantity-selector">
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="1"
+            value={quantity}
+            onChange={(e) => setQuantity(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="button-group">
+          <button className="cart-button" onClick={() => addCart(product.PR_ID,quantity)}>
+            Add to cart
+          </button>
+          <button className="checkout-button" onClick={handleCheckout}>
+            Checkout
+          </button>
+          <button
+            className="continue-shopping-button"
+            onClick={handleContinueShopping}
+          >
+            Continue Shopping
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
