53d44d7fd60b42978086eb5a1fde714b95f3edf3
Md. Tanvin Sarkar Pallab <tanvin.pallab2442002@gmail.com>
2024-07-16 01:22:10 +0600

    fetching delivery man and updating GET table.

--- Backend/Route/productDetails.js
+++ Backend/Route/productDetails.js
@@ -191,4 +191,46 @@ routerProduct.post('/products/detail/checkout/updateQuantity', async (req, res) 
   } 
 });
 
+routerProduct.get('/products/detail/checkout/deliveryman', async (req, res) => {
+    const { city } = req.query;
+    let connection;
+    try {
+        connection = await getConnection();
+        const result = await connection.execute(`
+            SELECT D_ID,NAME FROM DELIVERY
+            WHERE
+            LOWER(CITY) = LOWER(:city)`
+          , { city });
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).send({ error: 'Internal server error' });
+    }
+});
+
+routerProduct.post('/products/detail/checkout/setAddress', async (req, res) => {
+    let connection;
+    const {P_ID,D_ID,DELIVERY_DATE,CITY,STREET,HOUSE_NO} = req.body;
+    console.log(req.body);
+    try{
+       connection= await getConnection();
+       console.log("-------");
+       const result = await connection.execute(`INSERT INTO GET (P_ID, D_ID, DELIVERY_DATE, CITY, STREET, HOUSE_NO) 
+        VALUES (:P_ID,:D_ID,TO_DATE(:DELIVERY_DATE,'YYYY-MM-DD'),:CITY,:STREET,:HOUSE_NO)`,
+        {P_ID,D_ID,DELIVERY_DATE,CITY,STREET,HOUSE_NO},
+        {
+          autoCommit: true
+        }
+       );
+       res
+      .status(200)
+      .send({ message: "Delivery address updated successfully!", result });
+      console.log(res.status);
+    }
+    catch(error){
+      console.error('Error executing query:', error);
+      res.status(500).send({ error: 'Internal server error' });
+    }
+});
+
 module.exports = routerProduct;
