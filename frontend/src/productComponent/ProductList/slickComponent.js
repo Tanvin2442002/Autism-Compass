0639764a294c970450d4fa530a4c296b34491874
Md. Tanvin Sarkar Pallab <tanvin.pallab2442002@gmail.com>
2024-08-30 17:54:25 +0600

    trying to fix corousel...

--- frontend/src/productComponent/ProductList/slickComponent.js
+++ frontend/src/productComponent/ProductList/slickComponent.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./mainContent.css";
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import AddCart from "../AddCart";
 import { toast, ToastContainer } from "react-toastify";
 
 const SlickComponent = ({ products }) => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [PopUp, setPopUp] = useState(false);
-    const [productCards, setProductCards] = useState([]);
-    const navigate = useNavigate();
-    const localData = JSON.parse(localStorage.getItem("USER"));
-    useEffect(() => {
-        // console.log("Products:", products);
-        if (products.length > 0) {
-            setProductCards(products);
-            setLoading(false);
-            setError(false);
-            // console.log("Product cards:", productCards);
-        } else {
-            setError(true);
-        }
-    }, [products]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [PopUp, setPopUp] = useState(false);
+  const [productCards, setProductCards] = useState([]);
+  const navigate = useNavigate();
+  const localData = JSON.parse(localStorage.getItem("USER"));
+  useEffect(() => {
+    // console.log("Products:", products);
+    if (products.length > 0) {
+      setProductCards(products);
+      setLoading(false);
+      setError(false);
+      // console.log("Product cards:", productCards);
+    } else {
+      setError(true);
+    }
+  }, [products]);
 
-    const handleClick = () => {
-        setPopUp(true);
-    };
-    
+  const handleAddCart = () => {
+    setPopUp(true);
+  };
+
   const handleCancel = () => {
     setPopUp(false);
   };
@@ -85,83 +85,83 @@ const SlickComponent = ({ products }) => {
     setPopUp(false);
   };
 
-    const settings = {
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
+  const settings = {
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-    const listItems = productCards.map((item) =>
-        <div className="cardslick" key={item.PR_ID} onClick={() => handleClick(item.PR_ID)}>
+  const listItems = productCards.map((item) => (
+    <div>
+      <div className="cardslick">
+        <div className="cardslick-img">
+          <img src={item.SRC} alt={item.NAME} />
+        </div>
+        <div className="cardslick-content">
+          <h2>{item.NAME}</h2>
+          <p>{item.DESCRIPTION}</p>
+        </div>
+        <div className="card-footer">
+          <div className="card-button">
+            <AddCart price={item.PRICE} onClick={handleAddCart} />
+          </div>
+        </div>
         {PopUp && (
-            <div className="confirmation-dialog">
-              <div className="confirmation-dialog-content">
-                <p>Do you want to add this product to cart?</p>
-                <button className="confirm-btn" onClick={handlePopUp(item)}>
-                  Yes
-                </button>
-                <button className="cancel-btn" onClick={handleCancel}>
-                  No
-                </button>
-              </div>
+          <div className="confirmation-dialog">
+            <div className="confirmation-dialog-content">
+              <p>Do you want to add this product to cart?</p>
+              <button className="confirm-btn" onClick={handlePopUp(item)}>
+                Yes
+              </button>
+              <button className="cancel-btn" onClick={handleCancel}>
+                No
+              </button>
             </div>
-          )}
-            <div className="cardslick-img">
-                <img src={item.SRC} alt={item.NAME} />
-            </div>
-            <div className="cardslick-content">
-                <h2>{item.NAME}</h2>
-                <p>{item.DESCRIPTION}</p>
-            </div>
-            <div className="card-footer">
-                <div className="card-button">
-                    <AddCart price={item.PRICE} />
-                </div>
-            </div>
-        </div>
-    );
+          </div>
+        )}
+      </div>
+    </div>
+  ));
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: No products found</div>;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: No products found</div>;
 
-    return (
-        <div className='slider-container'>
-            <h1>Autism Compass Stationary</h1>
-            <Slider {...settings}>
-                {listItems}
-            </Slider>
-        </div>
-    );
+  return (
+    <div className="slider-container">
+      <h1>Autism Compass Stationary</h1>
+      <Slider {...settings}>{listItems}</Slider>
+    </div>
+  );
 };
 
 export default SlickComponent;
